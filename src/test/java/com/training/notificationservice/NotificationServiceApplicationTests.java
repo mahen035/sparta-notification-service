97f4d226fd88c8cbf5c183f17628198492a18193
mahen112 <mahen112@mail.com>
2026-07-16 09:18:13 +0530

    initial commit

---
+++ src/test/java/com/training/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
